f882d076c5e8db02342d03203c93e4153249e0f9
KGC31 <kimnguyenblc@gmail.com>
2024-09-19 15:21:53 +0700

    feat: manage status with concerns

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
-  has_many :comments, class_name: "Comment", foreign_key: "article_id" # Optional, use only if the foreign key is different
+  include Visible
+
+  has_many :comments, class_name: "Comment", foreign_key: "article_id", dependent: :destroy # Optional, use only if the foreign key is different
   # OR simply omit the foreign_key option if you're using Rails conventions
   
   validates :title, presence: true
