8080af0a702f73ec248af11ed0071a0fb91260ed
KGC31 <kimnguyenblc@gmail.com>
2024-09-19 14:29:33 +0700

    feat: add comment to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
-  has_many :comments, class_name: "comment", foreign_key: "reference_id"
-
+  has_many :comments, class_name: "Comment", foreign_key: "article_id" # Optional, use only if the foreign key is different
+  # OR simply omit the foreign_key option if you're using Rails conventions
+  
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
